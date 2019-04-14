47b1dd69fb709f904f13d47ac4bd0fdf5464a7c9
Emilio Nyaray <emilio.nyaray@valtech.se>
2019-04-14 10:58:57 +0200

    Needed moar robustness.

--- src/lib.rs
+++ src/lib.rs
@@ -1,52 +1,51 @@
-use std::io::Error as IOError;
+use std::io::{
+    Error as IOError,
+    ErrorKind::{InvalidData, InvalidInput},
+};
 
 type WaveFrame = (u64, f32);
 type WaveData = Vec<WaveFrame>;
 type WaveDataResult = Result<WaveData, IOError>;
 
 pub fn open_and_parse_csv(csv_path: &str) -> WaveDataResult {
-    let contents = std::fs::read_to_string(csv_path)?;
-    parse_csv(contents)
-}
-
-fn parse_csv(contents: String) -> Result<WaveData, IOError> {
-    let acc = Ok(vec![]);
-    contents
+    std::fs::read_to_string(csv_path)?
         .lines()
         .filter(is_non_empty)
-        .fold(acc, parse_csv_line)
-}
-
-fn parse_csv_line(res: WaveDataResult, line: &str) -> WaveDataResult {
-    match res {
-        Ok(mut acc) => match tokenise_line(line).map(parse_wave_data) {
-            Ok(line_data) => {
-                acc.push(line_data);
-                Ok(acc)
-            }
-            Err(e) => Err(e),
-        },
-        Err(_) => res,
-    }
+        .fold(Ok(vec![]), process_line) // TODO: maybe just do a for-loop with `break`?
 }
 
 fn is_non_empty<'r>(line: &'r &str) -> bool {
     !line.trim().is_empty()
 }
 
+fn process_line(res: WaveDataResult, line: &str) -> WaveDataResult {
+    res.and_then(|mut acc| {
+        parse_csv_line(line).map(|line_data| {
+            acc.push(line_data);
+            acc
+        })
+    })
+}
+
+fn parse_csv_line(line: &str) -> Result<WaveFrame, IOError> {
+    tokenise_line(line).and_then(parse_wave_data)
+}
+
 fn tokenise_line<'a>(line: &'a str) -> Result<(String, String), IOError> {
     let mut line_parts = line.split(",");
     let line_ts_raw = line_parts
         .next()
-        .ok_or_else(|| IOError::from(std::io::ErrorKind::InvalidInput))?;
+        .ok_or_else(|| IOError::from(InvalidInput))?;
     let line_amp_raw = line_parts
         .next()
-        .ok_or_else(|| IOError::from(std::io::ErrorKind::InvalidInput))?;
+        .ok_or_else(|| IOError::from(InvalidInput))?;
     Ok((line_ts_raw.to_string(), line_amp_raw.to_string()))
 }
 
-fn parse_wave_data((a, b): (String, String)) -> WaveFrame {
-    (a.parse().unwrap(), b.parse().unwrap())
+fn parse_wave_data((a, b): (String, String)) -> Result<WaveFrame, IOError> {
+    let a_val = a.parse().or_else(|_| Err(IOError::from(InvalidData)))?;
+    let b_val = b.parse().or_else(|_| Err(IOError::from(InvalidData)))?;
+    Ok((a_val, b_val))
 }
 
 #[cfg(test)]
@@ -67,7 +66,12 @@ mod tests {
     }
 
     #[test]
-    fn test_api_errors() {
-        assert!(open_and_parse_csv("asset/test_data_malformed.csv").is_err());
+    fn verify_bad_line_errors() {
+        assert!(open_and_parse_csv("asset/test_data_malformed_line.csv").is_err());
+    }
+
+    #[test]
+    fn verify_bad_data_errors() {
+        assert!(open_and_parse_csv("asset/test_data_malformed_data.csv").is_err());
     }
 }
